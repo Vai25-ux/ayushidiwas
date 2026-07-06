929df2c6448bc568d2812d6434e49d05cedc92d9
Vai25-ux <partner.edb@gmail.com>
2026-07-06 15:07:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".button").addEventListener("click", function(e){
+    e.preventDefault();
+    alert("🎉 Thank you! We can't wait to celebrate Ayushi Diwas with you! ❤️");
+});
